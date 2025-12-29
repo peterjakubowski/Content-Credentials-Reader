bb5122c2db7a6ecce6e39295d88f7488a404dc47
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2025-12-29 15:41:00 -0800

    Create streamlit_workflow.py

---
+++ components/streamlit_workflow.py
@@ -0,0 +1,15 @@
+from components.streamlit_metrics_section import streamlit_metrics_section
+from components.streamlit_main_section import streamlit_main_section
+from components.streamlit_origin_section import streamlit_origin_section
+from components.streamlit_actions_section import streamlit_actions_section
+
+
+def streamlit_workflow():
+
+    streamlit_metrics_section()
+
+    streamlit_main_section()
+
+    streamlit_origin_section()
+
+    streamlit_actions_section()

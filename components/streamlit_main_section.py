2633477abcd43487f824f75d2670a1e2383af24c
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-02-04 13:04:19 -0800

    Update streamlit_main_section.py

--- components/streamlit_main_section.py
+++ components/streamlit_main_section.py
@@ -18,17 +18,21 @@ def streamlit_main_section():
     # show validation state and signature information
     with col2:
         with st.container(border=True, width="stretch"):
-            # st.markdown("Validation State")
             if validation_state == "Valid":
                 st.markdown(f"**Validation State**: :green-badge[:material/check: {validation_state}]")
             elif validation_state == "Invalid":
+                validation_status = manifest_store.get("validation_status", [])
                 st.markdown(f"**Validation State**: :red-badge[:material/block: {validation_state}]")
+                for status in validation_status:
+                    st.markdown(f"**Code**: {status.get("code")}")
+                    st.markdown(f"**Explanation**: {status.get("explanation")}")
+
         with st.container(border=True):
             st.subheader("Signature Info", help="Signature information from active manifest.", anchor=False)
             # st.markdown(f"**alg**: {signature_info.get('alg')}")
             st.markdown(f"**Issuer**: {signature_info.get('issuer')}")
             st.markdown(f"**Name**: {signature_info.get('common_name')}")
-            st.markdown(f"**Time**: {signature_info.get('time')}")
+            st.markdown(f"**Date**: {signature_info.get('time')}")
     # display the image
     with col1:
         st.image(st.session_state.get('upload'))
